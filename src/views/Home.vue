388dc4584f25080b03438f8484d9214404bc6706
elkarroumytaha <elkarroumytaha@gmail.com>
2021-12-13 17:20:00 +0100

    Pushing the pr

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <Main />
+</template>
+
+<script>
+  import Main from '../components/Main'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      Main,
+    },
+  }
+</script>
